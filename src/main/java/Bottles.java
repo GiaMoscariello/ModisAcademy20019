258aee7e2c7ac86d81491655ab7300d0d3738a77
JimMinor <gia.moscariello@gmail.com>
2019-12-12 16:39:44 +0100

    added method actionAfterTakeBeer

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -12,25 +12,33 @@ class Bottles {
             if (beers == 0) {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Go to the store, buy some more,");
+                out.print(actionAfterTakeBeer(beers));
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
             } else if (beers == 1) {
                 out.print(beers + " bottle" + " of beer on the wall,");
                 out.println(beers + " bottle" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(actionAfterTakeBeer(beers));
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             } else if (beers == 2) {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(actionAfterTakeBeer(beers));
                 out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
             } else {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(actionAfterTakeBeer(beers));
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             }
+        }
+    }
+
+    public static String actionAfterTakeBeer(int beers) {
+        if(beers == 0) {
+            return "Go to the store, buy some more,";
+        } else {
+            return "Take one down, pass it around,";
         }
     }
 }
