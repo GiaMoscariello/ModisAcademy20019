23ff5bea4b534f2b7ad24c15db53e9506eb5ab7a
JimMinor <gia.moscariello@gmail.com>
2019-12-12 17:29:26 +0100

    less readable code

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -5,31 +5,13 @@ class Bottles {
 
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--) {
-            if (beers == 0) {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
-                out.print(actionAfterTakeBeer(beers));
-                out.println("99 bottles of beer on the wall.\r\n");
-                return;
-            } else if (beers == 1) {
-                out.print(beers + " bottle" + " of beer on the wall,");
-                out.println(beers + " bottle" + " of beer,");
-                out.print(actionAfterTakeBeer(beers));
-                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
-            } else if (beers == 2) {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
-                out.print(actionAfterTakeBeer(beers));
-                out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
-            } else {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
-                out.print(actionAfterTakeBeer(beers));
-                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
-            }
+            out.print(beers + container(beers) + " of beer on the wall,");
+            out.println(beers + container(beers) + " of beer,");
+            out.print(actionAfterTakeBeer(beers));
+            out.println(beersLeft(beers) + container(beers - 1) + " of beer on the wall.\r\n");
         }
-    }
 
+    }
 
     public static void main(String args[]) {
         writeLyrics(System.out);
@@ -37,5 +19,16 @@ class Bottles {
 
     private static String actionAfterTakeBeer(int beers) {
         return (beers == 0) ? "Go to the store, buy some more," : "Take one down, pass it around,";
+    }
+
+    private static String container(int beers) {
+        return (beers == 1) ?
+                " bottle" :
+                " bottles";
+    }
+
+    private static int beersLeft(int beers) {
+        return (beers + 99) % 100;
+
     }
 }
